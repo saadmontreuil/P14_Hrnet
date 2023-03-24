8c6da236efe59d4c25368fbd281703d6beb3d02e
saadmontreuil <79247132+saadmontreuil@users.noreply.github.com>
2023-03-24 18:03:04 +0100

    creating Header Component+ using datePicker  plugin

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.jpg';
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">
+        <h1>HRnet</h1>
+      </Link>
+      <div className={styles.logo}>
+        <img src={logo} alt="logo" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 import CustomDatePicker from '../../Components/CustomDatePicker/CustomDatePicker';
+import Header from '../../Components/Header/Header';
 
 export default function CreateEmployee() {
+  const [birthDate, setBirthDate] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const firstName = formData.get('firstname');
+    const lastName = formData.get('lastname');
+    const street = formData.get('street');
+    const city = formData.get('city');
+    const state = formData.get('state');
+    const zipCode = formData.get('zip-code');
+    const department = formData.get('department');
+
+    const employee = {
+      firstName,
+      lastName,
+      birthDate,
+      startDate,
+      address: {
+        street,
+        city,
+        state,
+        zipCode,
+      },
+      department,
+    };
+
+    const employees = JSON.parse(localStorage.getItem('employees')) || [];
+    employees.push(employee);
+    localStorage.setItem('employees', JSON.stringify(employees));
+  };
   return (
     <main className="create-employee">
+      <Header />
       <div className="header">
         <h2>Create Employee</h2>
         <Link to="/employees" className="to-employees">View Current Employees</Link>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <section className="firstname">
           <label htmlFor="firstname">First Name</label>
           <input
@@ -32,12 +67,12 @@ export default function CreateEmployee() {
 
         <section className="birth">
           <label htmlFor="birth">Date of Birth</label>
-          <CustomDatePicker />
+          <CustomDatePicker setDate={setBirthDate} />
         </section>
 
         <section className="start">
           <label htmlFor="start">Start Date</label>
-          <CustomDatePicker />
+          <CustomDatePicker setDate={setStartDate} />
         </section>
 
         <fieldset>
