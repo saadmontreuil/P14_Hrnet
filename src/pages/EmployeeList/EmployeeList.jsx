899adf3da29ebee8bae0f45bb5cef9d1f8c3a2c9
saadmontreuil <79247132+saadmontreuil@users.noreply.github.com>
2023-04-05 14:49:04 +0200

    Update EmployeeList.jsx

--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -1,57 +1,62 @@
-import React, { useMemo, useContext } from 'react';
+import React, { useContext } from 'react';
 import MaterialReactTable from 'material-react-table';
 import { EmployeeContext } from '../../hook/EmployeeContext';
 
 export default function EmployeeList() {
-  // const data = JSON.parse(localStorage.getItem('employees'));
   const { employees } = useContext(EmployeeContext);
 
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: 'firstName',
-        header: 'First Name',
-      },
-      {
-        accessorKey: 'lastName',
-        header: 'Last Name',
-      },
-      {
-        accessorKey: 'startDate',
-        header: 'Start Date',
-      },
-      {
-        accessorKey: 'department.label',
-        header: 'Department',
-      },
-      {
-        accessorKey: 'birthDate',
-        header: 'Date of Birth',
-      },
-      {
-        accessorKey: 'address.street',
-        header: 'Street',
-      },
-      {
-        accessorKey: 'address.city',
-        header: 'City',
-      },
-      {
-        accessorKey: 'address.state.label',
-        header: 'State',
-      },
-      {
-        accessorKey: 'address.zipCode',
-        header: 'Zip Code',
-      },
-    ],
-    [],
-  );
+  const columns = [
+    {
+      accessorKey: 'firstName',
+      header: 'First Name',
+    },
+    {
+      accessorKey: 'lastName',
+      header: 'Last Name',
+    },
+    {
+      accessorKey: 'startDate',
+      header: 'Start Date',
+    },
+    {
+      accessorKey: 'department.label',
+      header: 'Department',
+    },
+    {
+      accessorKey: 'birthDate',
+      header: 'Date of Birth',
+    },
+    {
+      accessorKey: 'address.street',
+      header: 'Street',
+    },
+    {
+      accessorKey: 'address.city',
+      header: 'City',
+    },
+    {
+      accessorKey: 'address.state.label',
+      header: 'State',
+    },
+    {
+      accessorKey: 'address.zipCode',
+      header: 'Zip Code',
+    },
+  ];
 
   return (
     <>
       <h2>Employee List</h2>
-      <MaterialReactTable data={employees} columns={columns} />
+      <MaterialReactTable
+        data={employees}
+        columns={columns}
+        enableColumnResizing
+        defaultColumn={{
+          maxSize: 400,
+          minSize: 80,
+          size: 150,
+        }}
+      />
     </>
   );
 }
